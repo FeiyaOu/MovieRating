41127d5bbd8aa48ccc7a09cfa8c09e7f03a7cae9
SophieOu16 <sophie16.ou@outlook.com>
2024-08-26 17:16:36 -0400

    feature updated https

--- src/App-version2.js
+++ src/App-version2.js
@@ -225,7 +225,7 @@ function MovieDetails({ selectedID, onClose, onAddWatched, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}`
         );
         const data = await res.json();
         setMovie(data);
